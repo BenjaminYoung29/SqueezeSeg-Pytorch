31e29297da305eed6abc647e52d51c8e4fa6eea1
Thrush911 <thrush.study@gmail.com>
2018-12-13 16:41:32 +0900

    Create v1-Config

---
+++ v1/src/config/kitti_squeezeSeg_config.py
@@ -0,0 +1,9 @@
+"""Model configuration for pascal dataset"""
+
+import numpy as np
+
+from config import base_model_config
+
+def kitti_squeezeSeg_config():
+	"""Specify the parameters to tune below."""
+
