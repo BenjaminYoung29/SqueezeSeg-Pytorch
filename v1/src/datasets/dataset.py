63e3edb75781359d829a2384a4e73da3fd7998e0
Thrush911 <thrush.study@gmail.com>
2019-01-25 17:32:17 +0900

    add datasets.py

---
+++ v1/src/datasets/dataset.py
@@ -0,0 +1,56 @@
+""" Kitti Dataset for SqueezeSeg """
+
+import pandas as pd
+
+import torch
+from torch.utils.data import Dataset
+
+import os
+import sys
+
+class KittiDataset(Dataset):
+    def __init__(self, mc, csv_file, root_dir, transform=None):
+        self.mc = mc
+
+        self.lidar_2d_csv = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.lidar_2d_csv)
+
+    def __getitem__(self, idx):
+        mc = self.mc
+        
+        lidar_name = os.path.join(self.root_dir, self.lidar_2d_csv.iloc[idx, 0])
+
+        lidar_data = np.load(lidar_name).astype(np.float32, copy=False)
+        
+        if mc.DATA_AUGMENTATION:
+            if mc.RANDOM_FLIPPING:
+                if np.random.rand() > 0.5:
+                    # flip y
+                    lidar_data = lidar_data[:, ::-1, :]
+                    lidar_data[:, :, 1] *= -1
+
+        lidar_inputs =  lidar_data[:, :, :5] # x, y, z, intensity, depth(range)
+
+        lidar_mask = np.reshape(
+            (lidar_inputs[:, :, 4] > 0),
+            [mc.ZENITH_LEVEL, mc.AZIMUTH_LEVEL, 1]
+        )
+
+        # Normalize Inputs
+        lidar_inputs = (lidar - mc.INPUT_MEAN) / mc.INPUT_STD
+        
+        lidar_label = lidar_data[:, :, 5]
+        
+        weight = np.zeros(lidar_label.shape)
+        for l in range(mc.NUM_CLASS):
+            weight[lidar_label == l] = mc.CLS_LOSS_WEIGHT[int(l)]
+        
+        if self.transform:
+            lidar_inputs = self.transform(lidar_inputs)
+            lidar_mask = self.transform(lidar_mask)
+
+        return (lidar_inputs, lidar_mask, torch.from_numpy(lidar_label), torch.from_numpy(weight))
